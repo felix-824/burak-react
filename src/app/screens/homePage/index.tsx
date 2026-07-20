0bfeb7d14bf7f7370926a4c0cc8b3d70d3153495
felix-824 <nf94new@gmail.com>
2026-07-21 03:08:56 +0900

    feat: develop product service business logic to retreive data from baackend server

--- src/app/screens/homePage/index.tsx
+++ src/app/screens/homePage/index.tsx
@@ -9,32 +9,36 @@ import "../../../css/home.css";
 
 import { useDispatch, useSelector } from "react-redux";
 import { Dispatch } from "@reduxjs/toolkit";
-import { createSelector } from "reselect";
 import { setPopularDishes } from "./slice";
 import { Product } from "../../../lib/types/product";
-import { retrievePopularDishes } from "./selector";
+import ProductService from "../../services/ProductService";
+import { Productcollection } from "../../../lib/enums/product.enum";
 
 /** REDUX SLICE & SELECTOR **/
 const actionDispatch = (dispatch: Dispatch) => ({
   setPopularDishes: (data: Product[]) => dispatch(setPopularDishes(data)),
 });
 
-const popularDishesRetriever = createSelector(
-  retrievePopularDishes,
-  (popularDishes) => ({popularDishes})
-);
 
 export default function HomePage() {
   const { setPopularDishes } = actionDispatch(useDispatch());
-  const { popularDishes } = useSelector(popularDishesRetriever);
 
-  console.log(process.env.REACT_APP_API_URL);
 
   useEffect(() => {
-   
+    const product = new ProductService();
+    product
+      .getProducts({
+        page: 1,
+        limit: 4,
+        order: "productViews",
+        productCollection: Productcollection.DISH,
+      })
+      .then((data) => {
+        setPopularDishes(data);
+      })
+      .catch((err) => console.log(err));
   }, []);
   
-
   return (
   <div className={"homepage"}>
     <Statistics/>
