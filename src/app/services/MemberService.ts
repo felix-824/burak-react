692ae12b232e193aaf1a36b18e9a529088aa12b4
felix-824 <nf94new@gmail.com>
2026-07-25 13:18:59 +0900

    fix: modify ChosenProduct component logic

--- src/app/services/MemberService.ts
+++ src/app/services/MemberService.ts
@@ -13,10 +13,24 @@ class MemberService {
         try {
             const url = this.path + "/member/top-users";
             const result = await  axios.get(url);
-            console.log("getYopUsers:", result);
+            console.log("getTopUsers:", result);
           return result.data;
         } catch(err) {
             console.log("Error, getTopUsers:", err);
+            throw err;
+        }
+    }
+
+     public async getRestaurant(): Promise<Member> {
+        try {
+            const url = this.path + "/member/restaurant";
+            const result = await  axios.get(url);
+            console.log("getRestaurant:", result);
+
+          const restaurant: Member = result.data;
+          return restaurant;
+        } catch(err) {
+            console.log("Error, getRestaurant:", err);
             throw err;
         }
     }
