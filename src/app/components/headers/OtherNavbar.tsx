197294e594fc6444ad182c7a024a34bca43cbaa3
felix-824 <nf94new@gmail.com>
2026-07-27 00:19:48 +0900

    fix: modify basket business logic via customized hooks

--- src/app/components/headers/OtherNavbar.tsx
+++ src/app/components/headers/OtherNavbar.tsx
@@ -6,10 +6,14 @@ import Basket from "./Basket";
 
 interface OtherNavbarProps {
   cartItems: CartItem[];
+  onAdd: (item: CartItem) => void;
+  onRemove: (item: CartItem) => void;
+  onDelete: (item: CartItem) => void;
+  onDeleteAll: () => void;
 }
 
 export default function OtherNavbar(props: OtherNavbarProps) {
-   const { cartItems } = props;
+   const { cartItems, onAdd, onRemove, onDelete, onDeleteAll } = props;
     const authMember = null;
     return (
      <div className="other-navbar">
@@ -40,7 +44,12 @@ export default function OtherNavbar(props: OtherNavbarProps) {
             <Box className={"hover-line"}>
              <NavLink to="/help" activeClassName={"underline"}>Help</NavLink>
            </Box>
-            <Basket cartItems={cartItems} />
+            <Basket cartItems={cartItems} 
+              onAdd={onAdd}
+              onRemove={onRemove}
+              onDelete={onDelete} 
+              onDeleteAll={onDeleteAll} 
+            />
 
             {!authMember ? (
                 <Box>
