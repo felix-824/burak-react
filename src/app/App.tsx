046a2b5dde18b4b94e0ee91d83a9a92144692031
felix-824 <nf94new@gmail.com>
2026-07-28 04:27:26 +0900

    feat: publish authentication modal component

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -8,18 +8,27 @@ import  UserPage  from "./screens/userPage";
 import HomeNavbar  from "./components/headers/HomeNavbar";
 import OtherNavbar  from "./components/headers/OtherNavbar";
 import  Footer  from "./components/footer";
-import "../css/app.css";
-import "../css/navbar.css";
-import "../css/footer.css";
 import  HelpPage  from "./screens/helpPage";
 import Test from "./screens/Test";
 import useBasket from "./hooks/useBasket";
+import AuthenticationModal from "./components/auth";
+import "../css/app.css";
+import "../css/navbar.css";
+import "../css/footer.css";
 
 
 function App() {
  const location = useLocation();
 
- const { cartItems, onAdd,  onRemove, onDelete, onDeleteAll, } = useBasket();
+  const { cartItems, onAdd,  onRemove, onDelete, onDeleteAll, } = useBasket();
+  const [signupOpen, setSignupOpen] = useState<boolean>(false);
+  const [loginOpen, setLoginOpen] = useState<boolean>(false);
+
+/** HANDLERS **/
+
+const handleSignupClose = () => setSignupOpen(false);
+const handleLoginClose = () => setLoginOpen(false);
+
 
   return ( 
     <>
@@ -56,6 +65,14 @@ function App() {
           </Route>
         </Switch>
         <Footer/>
+
+        <AuthenticationModal
+         signupOpen={signupOpen}
+         loginOpen={loginOpen}
+         handleLoginClose={handleLoginClose}
+         handleSignupClose={handleSignupClose}
+        />
+
       </>
   );
 }
