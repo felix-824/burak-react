72e59df68fa7d1f9442e16df0ae5e8e10634ce46
felix-824 <nf94new@gmail.com>
2026-07-28 06:21:31 +0900

    feat: create signup process

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -38,14 +38,21 @@ const handleLoginClose = () => setLoginOpen(false);
          onAdd={onAdd}
          onRemove={onRemove}
          onDelete={onDelete} 
-         onDeleteAll={onDeleteAll} /> 
+         onDeleteAll={onDeleteAll} 
+         setSignupOpen={setSignupOpen}
+         setLoginOpen={setLoginOpen}
+         /> 
+         
      ) : ( 
       <OtherNavbar
        cartItems={cartItems}
        onAdd={onAdd}
        onRemove={onRemove}
        onDelete={onDelete}
-       onDeleteAll={onDeleteAll} /> 
+       onDeleteAll={onDeleteAll} 
+       setSignupOpen={setSignupOpen}
+       setLoginOpen={setLoginOpen}
+       /> 
       )}
         <Switch>
           <Route path="/products">
