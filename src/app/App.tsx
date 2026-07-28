de4ce432e92d82627c034c3b2a815d8752d61a0b
felix-824 <nf94new@gmail.com>
2026-07-29 04:29:20 +0900

    feat: create useGlobals integrated with Context & develop logout process

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -15,19 +15,42 @@ import AuthenticationModal from "./components/auth";
 import "../css/app.css";
 import "../css/navbar.css";
 import "../css/footer.css";
+import { T } from "../lib/types/common";
+import { sweetErrorHandling, sweetTopSuccessAlert } from "../lib/sweetAlert";
+import { Messages } from "../lib/config";
+import MemberService from "./services/MemberService";
+import { useGlobals } from "./hooks/useGlobals";
 
 
 function App() {
  const location = useLocation();
-
+ const {setAuthMember} = useGlobals();
   const { cartItems, onAdd,  onRemove, onDelete, onDeleteAll, } = useBasket();
   const [signupOpen, setSignupOpen] = useState<boolean>(false);
   const [loginOpen, setLoginOpen] = useState<boolean>(false);
+  const [anchorEl, setAnchorEl] = useState<HTMLElement | null>(null);
 
 /** HANDLERS **/
 
 const handleSignupClose = () => setSignupOpen(false);
 const handleLoginClose = () => setLoginOpen(false);
+
+const handleLogoutClick = (e:  React.MouseEvent<HTMLElement>) => {
+  setAnchorEl(e.currentTarget);
+};
+const handleCloseLogout = () => setAnchorEl(null);
+const handleLoginRequest = async () => {
+  try {
+    const member = new MemberService();
+    await member.logout();
+
+    await sweetTopSuccessAlert("success", 700);
+    setAuthMember(null);
+  } catch(err) {
+    console.log(err);
+    sweetErrorHandling(Messages.error1);
+  }
+};
 
 
   return ( 
@@ -41,6 +64,10 @@ const handleLoginClose = () => setLoginOpen(false);
          onDeleteAll={onDeleteAll} 
          setSignupOpen={setSignupOpen}
          setLoginOpen={setLoginOpen}
+         anchorEl={anchorEl}
+         handleLogoutClick={handleLogoutClick}
+         handleCloseLogout={handleCloseLogout}
+         handleLoginRequest={handleLoginRequest}
          /> 
          
      ) : ( 
@@ -52,6 +79,10 @@ const handleLoginClose = () => setLoginOpen(false);
        onDeleteAll={onDeleteAll} 
        setSignupOpen={setSignupOpen}
        setLoginOpen={setLoginOpen}
+      anchorEl={anchorEl}
+      handleLogoutClick={handleLogoutClick}
+      handleCloseLogout={handleCloseLogout}
+      handleLoginRequest={handleLoginRequest}
        /> 
       )}
         <Switch>
--- src/app/components/headers/OtherNavbar.tsx
+++ src/app/components/headers/OtherNavbar.tsx
@@ -1,7 +1,16 @@
 /* eslint-disable jsx-a11y/alt-text */
-import { Box, Button, Container, Stack, } from "@mui/material";
+import { Box,
+   Button,
+   Container,
+   Stack, 
+   Menu, 
+   MenuItem,
+   ListItemIcon,} from "@mui/material";
+import { Logout } from "@mui/icons-material";
 import { NavLink } from "react-router-dom";
+import { serverApi } from "../../../lib/config";
 import { CartItem } from "../../../lib/types/search";
+import { useGlobals } from "../../hooks/useGlobals";
 import Basket from "./Basket";
 
 interface OtherNavbarProps {
@@ -12,6 +21,10 @@ interface OtherNavbarProps {
   onDeleteAll: () => void;
   setSignupOpen: (isOpen: boolean) => void;
   setLoginOpen: (isOpen: boolean) => void;
+  handleLogoutClick: (e: React.MouseEvent<HTMLElement>) => void;
+  anchorEl: HTMLElement | null;
+  handleCloseLogout: () => void;
+  handleLoginRequest: () => void;
 }
 
 export default function OtherNavbar(props: OtherNavbarProps) {
@@ -21,10 +34,14 @@ export default function OtherNavbar(props: OtherNavbarProps) {
           onDelete, 
           onDeleteAll,  
           setSignupOpen, 
-          setLoginOpen   
+          setLoginOpen,
+          handleLogoutClick,
+          anchorEl,
+          handleCloseLogout,
+          handleLoginRequest,   
         } = props;
-    const authMember = null;
-
+   // const authMember = null;
+   const { authMember } = useGlobals();
     return (
      <div className="other-navbar">
         <Container className="navbar-container">
@@ -74,10 +91,59 @@ export default function OtherNavbar(props: OtherNavbarProps) {
             ) : (
                 // eslint-disable-next-line jsx-a11y/role-supports-aria-props, jsx-a11y/alt-text
                 <img className="user-avatar" 
-                 src={"/icons/default-user.svg"} 
+                 src={
+                  authMember?.memberImage
+                  ? `${serverApi}/${authMember?.memberImage}`
+                 : "/icons/default-user.svg"
+                } 
                  aria-haspopup={"true"}
+                 onClick={handleLogoutClick}
                 />
             )}
+
+             <Menu
+              anchorEl={anchorEl}
+                id="account-menu"
+                open={Boolean(anchorEl)}
+                onClose={handleCloseLogout}
+                onClick={handleCloseLogout}
+                PaperProps={{
+                  elevation: 0,
+                  sx: {
+                    overflow: 'visible',
+                    filter: 'drop-shadow(0px 2px 8px rgba(0,0,0,0.32))',
+                    mt: 1.5,
+            '& .MuiAvatar-root': {
+              width: 32,
+              height: 32,
+              ml: -0.5,
+              mr: 1,
+            },
+            '&:before': {
+              content: '""',
+              display: 'block',
+              position: 'absolute',
+              top: 0,
+              right: 14,
+              width: 10,
+              height: 10,
+              bgcolor: 'background.paper',
+              transform: 'translateY(-50%) rotate(45deg)',
+              zIndex: 0,
+            },
+          },
+        }}
+        transformOrigin={{ horizontal: 'right', vertical: 'top' }}
+        anchorOrigin={{ horizontal: 'right', vertical: 'bottom' }}
+      >
+        <MenuItem onClick={handleLoginRequest}>
+          <ListItemIcon>
+            <Logout fontSize="small" style={{ color: 'blue' }} />
+          </ListItemIcon>
+          Logout
+        </MenuItem>
+
+      </Menu>
            </Stack>
          </Stack>
       </Container>  
